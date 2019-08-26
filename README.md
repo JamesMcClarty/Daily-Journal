79c48e7b23af92e06c2cb4570a4f38197d6b3722
unknown <mariobud@gmail.com>
2019-08-26 14:06:14 -0500

    added Javascript File and journalEntry class

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Daily-Journal
 
 ## Daily Journal 2: Added UI/UX
+
+## Daily Journal 3: Added Scripts
---
+++ scripts/journal.js
@@ -0,0 +1,10 @@
+const journalEntry = {
+    dateEntry ={
+        day = 00,
+        month = 00,
+        year = 0000
+    },
+    titleEntry = "",
+    journalEntry = "",
+    moodEntry = ""
+}
